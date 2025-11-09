98cf9fb648612c3ee2796ede8467d3043c09dc7f
Leidy J Aguedelo <leidyjohanna64@gmail.com>
2025-11-08 23:38:18 -0500

    Se corrige ruta

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import MessagePost from "./assets/pages/MessagePost.jsx"
 import Profile from "./assets/pages/Profile.jsx"
 import Mypets from "./assets/pages/Mypets.jsx"
 import Login from "./assets/pages/Login.jsx"
-import Notfound from "./assets/pages/NotFound.jsx"
+
 import Register from "./assets/pages/Register.jsx"
 import Volunteering from "./assets/pages/Volunteering.jsx"
 import Donations from "./assets/pages/Donations.jsx"
