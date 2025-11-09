7986af0e9622aafa871aec76a69dc06300436ae6
Leidy J Aguedelo <leidyjohanna64@gmail.com>
2025-11-08 23:29:26 -0500

    Corrige nombre de imagen notFound.jpg para despliegue en AWS

--- src/assets/pages/NotFound.jsx
+++ src/assets/pages/NotFound.jsx
@@ -1,12 +1,13 @@
 import ButtonHome from '../components/ui/ButtonHome'
-import notFoundImage from '../images/notFound.jpg'
+import notFound from '../images/notFound.jpg'
+
 
 const NotFound = () => {
     return (
         <>
             <div className='bg-[var(--creatdark)] flex flex-col items-center justify-center min-h-screen'>
                 <span className='italic text-white text-2xl p-10'>Página no encontrada</span>
-                <img src={notFoundImage} className='p20-5 m-5' alt="Página no encontrada" />
+                <img src={notFound} className='p20-5 m-5' alt="Página no encontrada" />
                 <span className='italic text-white text-2xl p-10'>¿Te has perdido?</span>
                 <ButtonHome />
             </div>
