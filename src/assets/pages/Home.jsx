da76738af7e457fae7be7b55e4577e693033c8d5
Leidy J Aguedelo <leidyjohanna64@gmail.com>
2025-11-08 23:04:11 -0500

    Se corrige ruta de imagen notFound

--- src/assets/pages/Home.jsx
+++ src/assets/pages/Home.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-
+import Header from "../components/layout/Header.jsx";
+import Footer from "../components/layout/Footer.jsx";
 const Home = () => {
     return (
         <>
