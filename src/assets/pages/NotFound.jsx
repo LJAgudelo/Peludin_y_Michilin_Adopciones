b229a7f382df37ec85d7be41102e9e4523bf29a1
Leidy J Aguedelo <leidyjohanna64@gmail.com>
2025-11-08 23:19:02 -0500

    Corrige conflicto en NotFound.jsx

--- src/assets/pages/NotFound.jsx
+++ src/assets/pages/NotFound.jsx
@@ -1,23 +1,17 @@
 import ButtonHome from '../components/ui/ButtonHome'
-import NotFound from '../images/notFound.jpg'
+import notFoundImage from '../images/notFound.jpg'
 
 const NotFound = () => {
     return (
         <>
-            <div className='bg-[var(--creatdark)] flex flex-col items-center justify-center  min-h-screen'>
-                <span className='italic text-white text-2xl p-10'>Página no encontrada </span>
-                <img src={NotFound} className='p20-5 m-5' />
-                <span className='italic text-white text-2xl p-10'>¿Te has perdido? </span>
-
+            <div className='bg-[var(--creatdark)] flex flex-col items-center justify-center min-h-screen'>
+                <span className='italic text-white text-2xl p-10'>Página no encontrada</span>
+                <img src={notFoundImage} className='p20-5 m-5' alt="Página no encontrada" />
+                <span className='italic text-white text-2xl p-10'>¿Te has perdido?</span>
                 <ButtonHome />
-
-            </div >
-
-
-
+            </div>
         </>
-
     )
 }
 
-export default NotFound  
+export default NotFound
